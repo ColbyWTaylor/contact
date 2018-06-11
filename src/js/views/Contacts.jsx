4939d962a8842e1d67e00cf52e5a0dc1e8e8f3c1
Alejandro Sanchez <aalejo@gmail.com>
2018-06-11 18:13:56 +0000

    solution for contact list

---
+++ src/js/views/Contacts.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Flux from "@4geeksacademy/react-flux-dash";
+import { Link } from "react-router-dom";
+
+import ContactCard from '../components/ContactCard';
+import avatar1 from '../../img/user_1.jpg';
+
+export default class Contacts extends Flux.View {
+    constructor(){
+        super();
+        this.state = {
+            //define your state properties here
+        };
+    }
+    render() {
+        return (
+            <div className="container">
+                <div>
+                    <p className="text-right my-3">
+                        <Link className="btn btn-success" to="/add">Add new contact</Link>
+                    </p>
+                    <div id="contacts" className="panel-collapse collapse show" aria-expanded="true">
+                        <ul className="list-group pull-down" id="contact-list">
+                            <ContactCard onDelete={(contact) => console.log('delete trash was clicked for: ',contact)} />
+                            <ContactCard />
+                            <ContactCard />
+                            <ContactCard />
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
